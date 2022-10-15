55895ea67ef53d16bb42200fdf8fbd5c54975c51
Tajul Islam Refath <tajul16-447@diu.edu.bd>
2022-10-15 22:09:21 +0600

    Import tinymce Text editor for write blog. and take input from form inputes

--- src/dashboard/user/CreatePost/CreatePost.jsx
+++ src/dashboard/user/CreatePost/CreatePost.jsx
@@ -1,18 +1,53 @@
 import "./createPost.scss";
+import { useRef, useEffect, useState } from "react";
+import { Editor } from "@tinymce/tinymce-react";
 
 import avatar from "../../../assets/img/thumbnail.jpg";
 import AppTitle from "../../../components/Common/AppTitle";
 
 const CreatePost = () => {
+  const [thumbnail, setThumbnail] = useState("");
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [tags, setTags] = useState("");
+  const [value, setValue] = useState("");
+
+  const editorRef = useRef(null);
+
+  const onThumbnailImage = (event) => {
+    const file = event.target.files[0];
+
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      setThumbnail(e.target.result);
+    };
+
+    reader.readAsDataURL(file);
+  };
+  const onSubmitPost = (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append("thumbnail", thumbnail);
+    formData.append("title", title);
+    formData.append("body", body);
+    formData.append("tags", tags);
+  };
+
   return (
     <>
       <AppTitle title="Dashboard-Create New Post" />
       <section className="createPost">
         <div className="container">
-          <form className="form">
+          <form
+            className="form"
+            onSubmit={onSubmitPost}
+            encType="multipart/form-data">
             <div className="col-md-10 offset-md-1">
               <div className="form__profile-pics">
-                <img src={avatar} alt="Avatar" className="image-thumble" />
+                {thumbnail && (
+                  <img src={thumbnail} alt="Avatar" className="image-thumble" />
+                )}
                 <label className="form__label" htmlFor="post-thumb">
                   Post thumbnail*
                 </label>
@@ -21,10 +56,11 @@ const CreatePost = () => {
                   name=" thumbnail"
                   id="post-thumb"
                   accept="image/*"
+                  onChange={onThumbnailImage}
                 />
               </div>
 
-              <div class="form-group">
+              <div className="form-group">
                 <label className="form__label" htmlFor="title">
                   Post Title*
                 </label>
@@ -33,6 +69,8 @@ const CreatePost = () => {
                   id="title"
                   className="form__input form-control"
                   placeholder="Enter a short title"
+                  value={title}
+                  onChange={(event) => setTitle(event.target.value)}
                 />
               </div>
 
@@ -40,10 +78,47 @@ const CreatePost = () => {
                 <label className="form__label" htmlFor="bio">
                   Post Body
                 </label>
-                <textarea
-                  class="form-control form__input"
-                  id="bio"
-                  rows="5"></textarea>
+                <Editor
+                  apiKey="your-api-key"
+                  value={value}
+                  onInit={(evt, editor) => (editorRef.current = editor)}
+                  onEditorChange={(value, editor) => {
+                    setValue(value);
+                    setBody(editor.getContent());
+                  }}
+                  initialValue="<p>Write your valuable words</p>"
+                  init={{
+                    height: 500,
+                    menubar: false,
+                    plugins: [
+                      "advlist",
+                      "autolink",
+                      "lists",
+                      "link",
+                      "image",
+                      "charmap",
+                      "preview",
+                      "anchor",
+                      "searchreplace",
+                      "visualblocks",
+                      "code",
+                      "fullscreen",
+                      "insertdatetime",
+                      "media",
+                      "table",
+                      "code",
+                      "help",
+                      "wordcount",
+                    ],
+                    toolbar:
+                      "undo redo | blocks | " +
+                      "bold italic forecolor | alignleft aligncenter " +
+                      "alignright alignjustify | bullist numlist outdent indent | " +
+                      "removeformat | help",
+                    content_style:
+                      "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+                  }}
+                />
               </div>
 
               <div class="form-group mb-5">
@@ -55,10 +130,14 @@ const CreatePost = () => {
                   id="tages"
                   className="form__input form-control"
                   placeholder="tag1, tag2 etc"
+                  value={tags}
+                  onChange={(event) => setTags(event.target.value)}
                 />
               </div>
 
-              <button className="form__btn dashboard-hover">Create</button>
+              <button type="submit" className="form__btn dashboard-hover">
+                Create
+              </button>
             </div>
           </form>
         </div>
--- src/dashboard/user/EditProfile/EditProfile.jsx
+++ src/dashboard/user/EditProfile/EditProfile.jsx
@@ -1,4 +1,6 @@
 import "./EditProfile.scss";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { FaInternetExplorer } from "react-icons/fa";
 import { AiFillLinkedin } from "react-icons/ai";
 import { AiFillGithub } from "react-icons/ai";
@@ -7,109 +9,114 @@ import avatar from "../../../assets/img/default.png";
 import AppTitle from "../../../components/Common/AppTitle";
 
 const EditProfile = () => {
+  const [profile, setProfile] = useState({});
+  const { myProfile } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (myProfile) {
+      setProfile(myProfile);
+    }
+  }, [myProfile]);
+
   return (
     <>
       <AppTitle title="Dashboard-Edit Profile" />
       <section className="editProfile">
         <div className="container">
-          <form className="form">
-            <div className="col-md-8 offset-md-3">
-              <h4 className="form__title">Update Profile</h4>
+          {profile && (
+            <form className="form">
+              <div className="col-md-8 offset-md-3">
+                <h4 className="form__title">Update Profile</h4>
 
-              <div className="form__profile-pics">
-                <img src={avatar} alt="Avatar" className="image-thumble" />
-                <input
-                  type="file"
-                  name="profilePics"
-                  id="profilePicFile"
-                  accept="image/*"
-                />
-              </div>
-
-              <div class="form-group">
-                <label className="form__label" for="name">
-                  Full Name*
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  className="form__input form-control"
-                  placeholder="Enter your name"
-                />
-              </div>
-
-              <div class="form-group">
-                <label className="form__label" for="email">
-                  Email address*
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  className="form__input form-control"
-                  placeholder="Enter your email"
-                />
-              </div>
-
-              <div class="form-group">
-                <label className="form__label" for="title">
-                  Enter A Short Title*
-                </label>
-                <input
-                  type="text"
-                  id="title"
-                  className="form__input form-control"
-                  placeholder="Enter a short title"
-                />
-              </div>
-
-              <div class="form-group">
-                <label className="form__label" for="bio">
-                  Enter A Short Bio*
-                </label>
-                <textarea
-                  class="form-control form__input"
-                  id="bio"
-                  rows="5"></textarea>
-              </div>
-              <label className="form__label">Social Links</label>
-              <div class="form-group icon-box">
-                <div className="form__icon">
-                  <FaInternetExplorer />
+                <div className="form__profile-pics">
+                  <img src={avatar} alt="Avatar" className="image-thumble" />
+                  <input
+                    type="file"
+                    name="profilePics"
+                    id="profilePicFile"
+                    accept="image/*"
+                  />
                 </div>
-                <input
-                  type="text"
-                  id="website"
-                  className="form__input form-control"
-                  placeholder="Website link"
-                />
-              </div>
-              <div class="form-group icon-box">
-                <div className="form__icon">
-                  <AiFillLinkedin />
-                </div>
-                <input
-                  type="text"
-                  id="linkdin"
-                  className="form__input form-control"
-                  placeholder="Website link"
-                />
-              </div>
 
-              <div class="form-group icon-box mb-5">
-                <div className="form__icon">
-                  <AiFillGithub />
+                <div class="form-group">
+                  <label className="form__label" for="name">
+                    Full Name*
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    className="form__input form-control"
+                    placeholder="Enter your name"
+                    value={profile.name}
+                  />
                 </div>
-                <input
-                  type="text"
-                  id="github"
-                  className="form__input form-control"
-                  placeholder="Website link"
-                />
-              </div>
 
-              <button className="form__btn dashboard-hover">Update</button>
-            </div>
-          </form>
+                <div class="form-group">
+                  <label className="form__label" for="title">
+                    Enter A Short Title*
+                  </label>
+                  <input
+                    type="text"
+                    id="title"
+                    className="form__input form-control"
+                    placeholder="Enter a short title"
+                    value={profile.title}
+                  />
+                </div>
+
+                <div class="form-group">
+                  <label className="form__label" for="bio">
+                    Enter A Short Bio*
+                  </label>
+                  <textarea
+                    class="form-control form__input"
+                    id="bio"
+                    rows="5"
+                    value={profile.bio}></textarea>
+                </div>
+                <label className="form__label">Social Links</label>
+                <div class="form-group icon-box">
+                  <div className="form__icon">
+                    <FaInternetExplorer />
+                  </div>
+                  <input
+                    type="text"
+                    id="website"
+                    className="form__input form-control"
+                    placeholder="Website link"
+                    value={profile.links?.website}
+                  />
+                </div>
+                <div class="form-group icon-box">
+                  <div className="form__icon">
+                    <AiFillLinkedin />
+                  </div>
+                  <input
+                    type="text"
+                    id="linkdin"
+                    className="form__input form-control"
+                    placeholder="Website link"
+                    value={profile.links?.linkdin}
+                  />
+                </div>
+
+                <div class="form-group icon-box mb-5">
+                  <div className="form__icon">
+                    <AiFillGithub />
+                  </div>
+                  <input
+                    type="text"
+                    id="github"
+                    className="form__input form-control"
+                    placeholder="Website link"
+                    value={profile.links?.github}
+                  />
+                </div>
+
+                <button className="form__btn dashboard-hover">Update</button>
+              </div>
+            </form>
+          )}
         </div>
       </section>
     </>
